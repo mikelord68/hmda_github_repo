f7875d6ab29c5001cc1cb3144f4dcdbc49e53808
mikelord68 <mikelord68@gmail.com>
2025-07-28 11:28:42 -0500

    Revert "Reordered Summary Results"
    This reverts commit 190d9ed1fc013f18557fa08180682d5f70c4ca25.

--- script.js
+++ script.js
@@ -137,13 +137,12 @@ function buildLARSummary(data) {
     return html;
   };
 
-return `
-  <h3>LAR Summary (${total} records)</h3>
-  ${summarize("Action Taken", actionTakenCounts, actionTakenLabels)}
-  ${summarize("Loan Purposes", loanPurposeCounts, loanPurposeLabels)}
-  ${summarize("Loan Types", loanTypeCounts, loanTypeLabels)}
-`;
-
+  return `
+    <h3>LAR Summary (${total} records)</h3>
+    ${summarize("Loan Types", loanTypeCounts, loanTypeLabels)}
+    ${summarize("Loan Purposes", loanPurposeCounts, loanPurposeLabels)}
+    ${summarize("Action Taken", actionTakenCounts, actionTakenLabels)}
+  `;
 }
 
 
